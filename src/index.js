bd7f78becdbc1a867d20e3412b33c9525ea6d3fa
rekunolya <rekun.olya@gmail.com>
2024-09-02 23:15:38 +0300

    feat: add solution to task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,16 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   let words = expr.split('**********');
+   let res ='';
+   for(let i = 0; i < words.length; i++){
+    for(let j = 0; j< words[i].length; j=j+10){
+        let str = words[i].slice(j, j + 10).replaceAll('11', '-').replaceAll('10', '.').replaceAll('0', '');
+        res += MORSE_TABLE[str];    
+    }
+    res += ' '; 
+   }
+  return res.substring(0, res.length - 1);
 }
 
 module.exports = {
